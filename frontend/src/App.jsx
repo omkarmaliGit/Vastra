eec567e2f409950a10e2a04a005237110937cd3b
omkarmaliGit <omkarmali51@gmail.com>
2025-03-21 05:50:40 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
-import Collection from "./pages/collection";
+import Home from "./pages/Home";
+import Collection from "./pages/Collection";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Product from "./pages/Product";
@@ -13,7 +13,7 @@ import ShopContextProvider from "./context/ShopContext";
 import Footer from "./components/Footer";
 import SearchBar from "./components/SearchBar";
 
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Verify from "./pages/Verify";
 
